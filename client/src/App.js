4bea636b0ce7b556ca51c18735d115fe53ec48d3
Tejas Sathe <tejassathe@Tejass-Air.lan>
2024-01-02 17:08:49 -0500

    Add tailwind css

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="App">
+      <main className="relative">
+        <section className="xl:padding-l wide-padding-r">
+
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
